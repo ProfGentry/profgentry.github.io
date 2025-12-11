dc4c46f3048314ef68d901088d253eb6ef318038
Jennifer Gentry <jennifer@gentrytechnology.com>
2025-12-11 14:19:18 -0500

    Update lab.js

--- ecpi/cyber-labs/parrot-hyperv-lab/lab.js
+++ ecpi/cyber-labs/parrot-hyperv-lab/lab.js
@@ -1,4 +1,4 @@
-function openTab(evt, tabName) {
+        function openTab(evt, tabName) {
             var tabContents = document.getElementsByClassName("tab-content");
             for (var i = 0; i < tabContents.length; i++) {
                 tabContents[i].classList.remove("active");
